4b2c6566975b9ae6ae9c88953f6d730039d9e04d
Julie Gravier <JGravier@users.noreply.github.com>
2023-01-19 17:15:55 +0100

    creation of csv as full date joining

---
+++ binding_csv.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+sequence_dates <- seq(1955, 2019, 1)
+
+tableausortie <- tibble(rank = numeric(), company = character(), revenue=numeric(), date = numeric())
+
+for (i in 1:length(x = sequence_dates)) {
+  lecture <- read.csv(file = paste0("csv/fortune500-", sequence_dates[i], ".csv")) %>%
+    as_tibble() %>%
+    rename(revenue=revenue....millions.)
+  lecture <- lecture[,1:3]
+  lecture <- lecture %>%
+    mutate(date = sequence_dates[i])
+  tableausortie <- tableausortie %>%
+    bind_rows(lecture)
+}
+
+tableausortie
+
+write.csv(x = tableausortie, file = "fortune500.csv")
